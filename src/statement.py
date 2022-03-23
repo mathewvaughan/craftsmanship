f24677fafae00d6bebb0b191407e6ca4d19a5bc7
Mathew Vaughan <mathew.vaughan@eigentech.com>
2022-03-23 21:52:33 +0000

    extract volume credits method

--- src/statement.py
+++ src/statement.py
@@ -25,17 +25,18 @@ def statement(invoice, plays):
             raise ValueError("Unknown Play type: %s".format(play_for(perf)["type"]))
         return result
 
-   
-    
-    for perf in invoice["performances"]:
-        this_amount = amount_for(perf)
-        # Add volume credits
-        volume_credits+=max(perf['audience']-30,0)
+    def volume_credits_for(perf):
+        result = max(perf['audience']-30,0)
         # add extra credit for every ten comedy attendees
         if play_for(perf)["type"] == "comedy":
-            volume_credits += int(perf["audience"]/5)
-        result +=f"  {play_for(perf)['name']} : {format(this_amount/100)} ({perf['audience']} seats)\n"
-        total_amount += this_amount
+            result += int(perf["audience"]/5)
+        return result
+    
+    for perf in invoice["performances"]:
+        # Add volume credits
+        volume_credits+=volume_credits_for(perf)
+        result +=f"  {play_for(perf)['name']} : {format(amount_for(perf)/100)} ({perf['audience']} seats)\n"
+        total_amount += amount_for(perf)
     result+= f"Amount owed is {format(total_amount/100)}\n"
     result+= f"You earned {volume_credits} credits\n"
     return result
