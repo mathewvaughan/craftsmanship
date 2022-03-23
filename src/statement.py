b23120c1388f0196ae404a2abbf5ae820711ea26
Mathew Vaughan <mathew.vaughan@eigentech.com>
2022-03-23 22:13:26 +0000

    extract total amount into method

--- src/statement.py
+++ src/statement.py
@@ -3,46 +3,49 @@ from babel.numbers import format_currency
 def statement(invoice, plays):
     usd = lambda x : format_currency(x, 'USD', locale='en_US')
 
-    def play_for(perf):
-        return plays[perf["playID"]]
+    def play_for(performance):
+        return plays[performance["playID"]]
 
     def amount_for(performance):
         result=0
-        if play_for(perf)["type"] == "tragedy":
+        if play_for(performance)["type"] == "tragedy":
             result=40000
             if performance["audience"] > 30:
                 result += 1000*(performance['audience']-30)
-        elif play_for(perf)["type"] == "comedy":
+        elif play_for(performance)["type"] == "comedy":
             result = 30000
             if performance["audience"] > 20:
                 result +=10000+500*(performance['audience'] - 20)
             result += 300 * performance["audience"]
-        if play_for(perf)["type"] not in {"tragedy", "comedy"}:
-            raise ValueError("Unknown Play type: %s".format(play_for(perf)["type"]))
+        if play_for(performance)["type"] not in {"tragedy", "comedy"}:
+            raise ValueError("Unknown Play type: %s".format(play_for(performance)["type"]))
         return result
 
-    def volume_credits_for(perf):
-        result = max(perf['audience']-30,0)
-        if play_for(perf)["type"] == "comedy":
-            result += int(perf["audience"]/5)
+    def volume_credits_for(performance):
+        result = max(performance['audience']-30,0)
+        if play_for(performance)["type"] == "comedy":
+            result += int(performance["audience"]/5)
         return result
 
     def total_volume_credits():
         volume_credits=0
-        for perf in invoice["performances"]:
-            volume_credits+=volume_credits_for(perf)
+        for performance in invoice["performances"]:
+            volume_credits+=volume_credits_for(performance)
         return volume_credits
     
     result = f"Statement for {invoice['customer']}\n"
     
-    total_amount=0
-    for perf in invoice["performances"]:
-        total_amount += amount_for(perf)
+    def total_amount():
+        result=0
+        for performance in invoice["performances"]:
+            result += amount_for(performance)
+        return result
+
     for perf in invoice["performances"]:
         result +=f"  {play_for(perf)['name']} : {usd(amount_for(perf)/100)} ({perf['audience']} seats)\n"
         
     
-    result+= f"Amount owed is {usd(total_amount/100)}\n"
+    result+= f"Amount owed is {usd(total_amount()/100)}\n"
     result+= f"You earned {total_volume_credits()} credits\n"
     return result
 
