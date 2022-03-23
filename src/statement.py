eedd2f05a7211be513ea72aefea62605d5c30885
Mathew Vaughan <mathew.vaughan@eigentech.com>
2022-03-23 21:43:09 +0000

    inline variable play with call to play_for

--- src/statement.py
+++ src/statement.py
@@ -3,33 +3,39 @@ from babel.numbers import format_currency
 def statement(invoice, plays):
     total_amount=0
     volume_credits=0
+
     result = f"Statement for {invoice['customer']}\n"
     format = lambda x : format_currency(x, 'USD', locale='en_US')
+
+    def play_for(perf):
+        return plays[perf["playID"]]
+
+    def amount_for(performance, play):
+        result=0
+        if play["type"] == "tragedy":
+            result=40000
+            if performance["audience"] > 30:
+                result += 1000*(performance['audience']-30)
+        elif play["type"] == "comedy":
+            result = 30000
+            if performance["audience"] > 20:
+                result +=10000+500*(performance['audience'] - 20)
+            result += 300 * performance["audience"]
+        if play["type"] not in {"tragedy", "comedy"}:
+            raise ValueError("Unknown Play type: %s".format(play["type"]))
+        return result
+
+   
+    
     for perf in invoice["performances"]:
-        play = plays[perf["playID"]]
-        this_amount = amount_for(perf, play)
+        this_amount = amount_for(perf, play_for(perf))
         # Add volume credits
         volume_credits+=max(perf['audience']-30,0)
         # add extra credit for every ten comedy attendees
-        if play["type"] == "comedy":
+        if play_for(perf)["type"] == "comedy":
             volume_credits += int(perf["audience"]/5)
-        result +=f"  {play['name']} : {format(this_amount/100)} ({perf['audience']} seats)\n"
+        result +=f"  {play_for(perf)['name']} : {format(this_amount/100)} ({perf['audience']} seats)\n"
         total_amount += this_amount
     result+= f"Amount owed is {format(total_amount/100)}\n"
     result+= f"You earned {volume_credits} credits\n"
-    return result
-
-def amount_for(perf, play):
-    result=0
-    if play["type"] == "tragedy":
-        result=40000
-        if perf["audience"] > 30:
-            result += 1000*(perf['audience']-30)
-    elif play["type"] == "comedy":
-        result = 30000
-        if perf["audience"] > 20:
-            result +=10000+500*(perf['audience'] - 20)
-        result += 300 * perf["audience"]
-    if play["type"] not in {"tragedy", "comedy"}:
-        raise ValueError("Unknown Play type: %s".format(play["type"]))
     return result
